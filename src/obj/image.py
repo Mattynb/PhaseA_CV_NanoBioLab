0b55aacd87883ed7ab1a74229e57e48202cb90b6
matheus <matheus.berbet001@umb.edu>
2023-11-06 11:05:43 -0500

    find_grid works

--- src/obj/image.py
+++ src/obj/image.py
@@ -6,9 +6,12 @@ class Image:
         
         self.img_og = image
         self.img_std_size = self.resize_2_std(image)
+        self.ref_squares = self.find_ref_squares(self.img_std_size)  # [coords1, cords2, ... , coords4]
+        self.find_grid()
+        #self.img_std_angle = self.angle_fix(image)
+
         #self.img_std = self.pre_process()  # std == processed
         
-        #self.ref_squares = self.find_ref_squares()  # [coords1, cords2, ... , coords4]
         #self.grid = None
         #self.edges = []
         #self.blocks = []
@@ -24,6 +27,39 @@ class Image:
 
         return img
 
+    def find_grid(self):
+        # Finding the corners of the grid.
+        tl,tr,bl,br = 0,0,0,0
+        for i in range (0, 4):
+            ref_sq = self.ref_squares[i]  # ref_squares is in order: [br, bl, tr, tl]
+            x,y,w,h = ref_sq
+
+            if i == 0:
+                bl = (x, y + h)
+
+            if i == 1:
+                br = (x + w, y + h)
+
+            if i == 2:
+                tl = (x, y)
+              
+            if i == 3: 
+                tr = (x + w, y)      
+                
+        corners = (tl, tr, bl, br)
+
+        # Visuals
+        '''
+        print(corners)
+        cv.line(self.img_std_size, tl, tr, (0,255,0), 2)
+        cv.line(self.img_std_size, tr, br, (0,255,0), 2)
+        cv.line(self.img_std_size, br, bl, (0,255,0), 2)
+        cv.line(self.img_std_size, bl, tl, (0,255,0), 2)
+        cv.imshow('img', self.img_std_size)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+        #'''
+        return corners
 
     def angle_fix(self):
         """ 
@@ -32,14 +68,7 @@ class Image:
         refer to the whole grid rather than individual squares
         """
         from numpy import sqrt, float32
-
-        for i in range (4):
-            ref_sq = self.ref_squares[i]
-            x,y,w,h = ref_sq
-            tl = (x,y) 
-            tr = (x+w, y) 
-            br = (x, y+h)
-            bl = (x+w, y+h)
+        
         
         
         # Finding the maximum width.
@@ -56,38 +85,37 @@ class Image:
         destination_corners = [[0, 0], [maxWidth, 0], [maxWidth, maxHeight], [0, maxHeight]]
 
         # Getting the homography.
-        #M = getPerspectiveTransform(float32(corners), float32(destination_corners))
+        M = cv.getPerspectiveTransform(float32(corners), float32(destination_corners))
 
         # Perspective transform using homography.
-        #final = warpPerspective(self.img_og, M, (destination_corners[2][0], destination_corners[2][1]), flags=INTER_LINEAR)
+        final = cv.warpPerspective(self.img_og, M, (destination_corners[2][0], destination_corners[2][1]), flags=INTER_LINEAR)
 
-        
-
-
-    def find_ref_squares(self):
+    def find_ref_squares(self, src_img):
         ref_squares = []
-        src_img = self.img_std_size
-
-        print(f"type(img):{type(src_img)}")
 
         # use threshold to leave only the black squares, there should be exactly four
-        img = cv.cvtColor(src_img, cv.COLOR_RGB2GRAY)
-        img = cv.GaussianBlur(img, (5,5), 9)
-        _, img = cv.threshold(img, 50, 255, cv.THRESH_BINARY)
-        edges = cv.Canny(img, 150, 150)
-
+        src_img = cv.cvtColor(src_img, cv.COLOR_RGB2GRAY)
+        src_img = cv.GaussianBlur(src_img, (5,5), 9)
+        _, src_img = cv.threshold(src_img, 50, 255, cv.THRESH_BINARY)
+        edges = cv.Canny(src_img, 150, 150)
+        
+        
         contours, _ = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-        img = self.img_std_size
+        
+        # Visuals
+        img_std = self.img_std_size
+        
         for contour in contours:
             x, y, w, h = cv.boundingRect(contour)
-            cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-
-        cv.imshow('img', img)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-
+            ref_squares.append((x,y,w,h))
+            cv.rectangle(img_std, (x,y), (x+5, y+5), (0,255,0), 2)
     
+            """
+            cv.imshow('img', img_std)
+            cv.waitKey(0)
+            cv.destroyAllWindows()
+            """
+
         # record the coordinates of each square edge
         # now you can calculate the lenght and area in pixels of each square
     
@@ -98,6 +126,7 @@ class Image:
 
     def resize_2_std(self, img):
         return cv.resize(img, (500,500))
+
 
 def Image_loader(path_to_images: str) -> list[Image]:
     """Loads all the images in a path as Image"""
@@ -125,11 +154,10 @@ def Image_loader(path_to_images: str) -> list[Image]:
 
 
 if __name__ == '__main__':
-    
+    # maybe before resizing, you should "cut" the image into a square shape to avoid distortion.
     image = Image_loader(r"C:\Users\Matheus\Desktop\NanoTechnologies_Lab\Phase A\data\img\block_with_refSq.jpeg") 
     
     I = image[0]
-    I.find_ref_squares()
 
     cv.waitKey(0)
     cv.destroyAllWindows()
