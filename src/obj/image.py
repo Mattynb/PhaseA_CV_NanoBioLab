7c98d83068d37aa7ccf7a13abc13bbfe20c8c9c0
matheus <matheus.berbet001@umb.edu>
2023-11-06 15:38:21 -0500

    Implemented Scanner

--- src/obj/image.py
+++ src/obj/image.py
@@ -8,10 +8,8 @@ class Image:
         
         self.img_og = image
         self.img_std_size = self.resize_2_std(image)
-        self.ref_squares = self.find_ref_squares(self.img_std_size) #self.img_std_size) # [coords1, cords2, ... , coords4]
         
-        corners = self.find_grid() #self.grid
-        self.img_std_angle = self.angle_fix(corners)
+        self.img_std = self.scan()
 
         #self.img_std = self.pre_process()  # std == processed
     
@@ -28,122 +26,134 @@ class Image:
 
         return img
 
-    def find_grid(self):
-        # Finding the corners of the grid.
-        tl,tr,bl,br = 0,0,0,0
-        for i in range (0, 4):
-            ref_sq = self.ref_squares[i]  # ref_squares is in order: [br, bl, tr, tl]
-            x,y,w,h = ref_sq
+    def scan(self):
+        """https://learnopencv.com/automatic-document-scanner-using-opencv/"""
+        img = self.img_std_size.copy()
+        #"""
+        cv.imshow('og', img)
+        cv.waitKey(0)  
+        cv.destroyAllWindows()
+        #"""
 
-            if i == 0:
-                bl = (x -(0.12*w), y + h+(0.12*h))
+        
+        # morphological operations ("removing" everything inside the grid)
+        kernel = np.ones((5,5), np.uint8)
+        img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel, iterations=3)  
+        #"""
+        cv.imshow('morph', img)
+        cv.waitKey(0)  
+        cv.destroyAllWindows()
+        #"""
 
-            if i == 1:
-                br = (x + w +(0.12*w), y + h +(0.12*h))
+        # GETTING RID OF THE BKG
+        mask = np.zeros(img.shape[:2],np.uint8)
+        bgdModel = np.zeros((1,65),np.float64)
+        fgdModel = np.zeros((1,65),np.float64)
+        rect = (20,20,img.shape[1]-20,img.shape[0]-20)
+        cv.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
+        mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+        img = img*mask2[:,:,np.newaxis]
+        #"""
+        cv.imshow('no_bkg', img)
+        cv.waitKey(0)  
+        cv.destroyAllWindows()
+        #"""
 
-            if i == 2:
-                tl = (x -(0.12*w), y -(0.12*h))
-              
-            if i == 3: 
-                tr = (x + w +(0.12*w), y -(0.12*h))      
-                
-        corners = [tl, tr, br, bl]
+        # EDGE DETECTION
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        gray = cv.GaussianBlur(gray, (11, 11), 0)
+        canny = cv.Canny(gray, 0, 200)
+        canny = cv.dilate(canny, cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5)))
 
-        # Visuals
-        """
-        print(corners)
-        cv.line(self.img_std_size, tl, tr, (255,255,0), 2)
-        cv.line(self.img_std_size, tr, br, (0,255,0), 2)
-        cv.line(self.img_std_size, br, bl, (0,255,255), 2)
-        cv.line(self.img_std_size, bl, tl, (0,0,255), 2)
-        cv.imshow('img', self.img_std_size)
+        # CONTOUR DETECTION
+        con = np.zeros_like(img)  # Blank canvas.
+        # Finding contours for the detected edges.
+        contours, hierarchy = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+        # Keeping only the largest detected contour.
+        page = sorted(contours, key=cv.contourArea, reverse=True)[:5]
+        con = cv.drawContours(con, page, -1, (0, 255, 255), 3)
+        #"""
+        cv.imshow('contour', con)
+        cv.waitKey(0)  
+        cv.destroyAllWindows()
+        #"""
+
+        # DETECTING THE CORNERS
+        con = np.zeros_like(img) # Blank canvas.
+        # Loop over the contours.
+        for c in page:
+            # Approximate the contour.
+            epsilon = 0.02 * cv.arcLength(c, True)
+            corners = cv.approxPolyDP(c, epsilon, True)
+            # If our approximated contour has four points
+            if len(corners) == 4:
+                break
+        cv.drawContours(con, c, -1, (0, 255, 255), 3)
+        cv.drawContours(con, corners, -1, (0, 255, 0), 10)
+        # Sorting the corners and converting them to desired shape.
+        corners = sorted(np.concatenate(corners).tolist())
+        corners = self.order_points(corners)
+        # Displaying the corners.
+        for index, c in enumerate(corners):
+            character = chr(65 + index)
+            cv.putText(con, character, tuple(c), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv.LINE_AA)
+        #"""
+        cv.imshow('contour', con)
+        cv.waitKey(0)  
+        cv.destroyAllWindows()
+        #"""
+
+        # REARRANGING THE CORNERS
+        destination_corners = self.find_dest(corners)
+
+        # Getting the homography.
+        M = cv.getPerspectiveTransform(np.float32(corners), np.float32(destination_corners))
+        # Perspective transform using homography.
+        final = cv.warpPerspective(self.img_std_size, M, (destination_corners[2][0], destination_corners[2][1]), flags=cv.INTER_LINEAR)
+        #"""
+        cv.imshow('final', final)
         cv.waitKey(0)
         cv.destroyAllWindows()
         #"""
-        return corners
+        return final
 
-    def angle_fix(self, corners):
-        """
-        Not Finished yet.\n
-        Be aware that the copied part of the algorithm assumes that tl, tr, br, bl 
-        refer to the whole grid rather than individual squares
-        """
-        
-        (tl, tr, bl, br) = corners 
-        
+    def find_dest(self, pts):
+        # DESTINATION COORDINATES
+        (tl, tr, br, bl) = pts
         # Finding the maximum width.
         widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
         widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
         maxWidth = max(int(widthA), int(widthB))
-
         # Finding the maximum height.
         heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
         heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
         maxHeight = max(int(heightA), int(heightB))
-
         # Final destination co-ordinates.
-        destination_corners = [[0, 0], [maxWidth, 0], [maxWidth, maxWidth], [0, maxWidth]]
-
-        # Getting the homography.
-        M = cv.getPerspectiveTransform(np.float32(corners), np.float32(destination_corners))
-
-        # Perspective transform using homography.
-        final = cv.warpPerspective(self.img_std_size, M, (destination_corners[2][0], destination_corners[2][1]), flags=cv.INTER_LINEAR)
-
-        cv.imshow('img', final)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-
-    def find_ref_squares(self, src_img):
-        # use threshold to leave only the black squares, there should be exactly four
-        src_img = cv.cvtColor(src_img, cv.COLOR_RGB2GRAY)
-        src_img = cv.GaussianBlur(src_img, (5,5), 9)
-        #src_img =cv.medianBlur(src_img, 5) # medianBlur is better for removing salt and pepper noise
-        #src_img =cv.blur(src_img, (5,5)) # blur is better for removing gaussian noise
-        #src_img =cv.stackBlur(src_img, 5) # stackBlur is better for removing gaussian noise
-        #src_img =cv.bilateralFilter(src_img, 5, 50, 50) # bilateralFilter is better for removing gaussian noise
-        _, src_img = cv.threshold(src_img, 50, 255, cv.THRESH_BINARY)
-        edges = cv.Canny(src_img, 150, 150)
-        
-        #"""
-        cv.imshow('img', src_img)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-        #"""
-        contours, _ = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        
-        # Visuals
-        img_std = self.img_std_size.copy()
-        
-        # find the four biggest squares
-        ref_squares = [(0,0,0,0),(0,0,0,0),(0,0,0,0),(0,0,0,0)]
-        for contour in contours:
-            x, y, w, h = cv.boundingRect(contour)
-
-            # if the contour is too small, it's probably not a square
-            if len(ref_squares) < 4 or w > min(ref_squares, key=lambda x: x[2])[2]:     # x is a tuple (x,y,w,h), x[2] is the width
-                ref_squares.remove(min(ref_squares, key=lambda x: x[2]))
-                ref_squares.append((x,y,w,h))
-
-        # paint the squares on the image
-        #"""
-        for x,y,w,h in ref_squares:
-            cv.rectangle(img_std, (x,y), (x+w, y+h), (255,0,0), 2)
-        
-            
-        cv.imshow('img', img_std)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-            #"""
-
-        # record the coordinates of each square edge
-        # now you can calculate the lenght and area in pixels of each square
+        destination_corners = [[0, 0], [maxWidth, 0], [maxWidth, maxHeight], [0, maxHeight]]
+        return self.order_points(destination_corners)
     
-        return ref_squares # [coords1, cords2, ... , coords4], coords = (x, y, L)
-
-    def resize_2_std(self, img):
+    def order_points(self, pts):
+        '''Rearrange coordinates to order:
+        top-left, top-right, bottom-right, bottom-left'''
+        rect = np.zeros((4, 2), dtype='float32')
+        pts = np.array(pts)
+        s = pts.sum(axis=1)
+        # Top-left point will have the smallest sum.
+        rect[0] = pts[np.argmin(s)]
+        # Bottom-right point will have the largest sum.
+        rect[2] = pts[np.argmax(s)]
+    
+        diff = np.diff(pts, axis=1)
+        # Top-right point will have the smallest difference.
+        rect[1] = pts[np.argmin(diff)]
+        # Bottom-left will have the largest difference.
+        rect[3] = pts[np.argmax(diff)]
+        # Return the ordered coordinates.
+        return rect.astype('int').tolist()
+  
+    def resize_2_std(self, img, factor=0.3):
         h, w = img.shape[:2]
-        return cv.resize(img, (int(w*0.3), int(h*0.3)), interpolation=cv.INTER_NEAREST)
+        return cv.resize(img, (int(w*factor), int(h*factor)), interpolation=cv.INTER_LINEAR)
 
 
 def Image_loader(path_to_images: str) -> list[Image]:
@@ -172,7 +182,7 @@ def Image_loader(path_to_images: str) -> list[Image]:
 
 
 if __name__ == '__main__':
-    image = Image_loader(r"C:\Users\Matheus\Desktop\NanoTechnologies_Lab\Phase A\data\img\block_with_refSq_") 
+    image = Image_loader(r"C:\Users\Matheus\Desktop\NanoTechnologies_Lab\Phase A\data\img\grid") 
     I = image[0]
 
     cv.waitKey(0)
